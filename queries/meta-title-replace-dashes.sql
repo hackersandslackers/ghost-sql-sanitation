c82d49c5dec88cdc4e7cec33c39e0c172fedcdb1
Todd Birchard <toddbirchard@gmail.com>
2020-01-20 00:34:28 -0500

    modified queries

--- queries/meta-title-replace-dashes.sql
+++ queries/meta-title-replace-dashes.sql
@@ -1,7 +1,6 @@
 UPDATE
 	posts_meta
 SET
-	meta_title = CONCAT(meta_title, ' - Hackers and Slackers')
+	meta_title = REPLACE(meta_title, ' - Hackers and Slackers', '')
 WHERE
-	meta_title IS NOT NULL
-	AND meta_title NOT LIKE '%%Hackers and Slackers%%';
+	meta_title IS NOT NULL;
